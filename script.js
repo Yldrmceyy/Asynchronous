652eabee9fff49a57513bcd7b245d3b2e51731f9
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-23 15:43:41 +0300

    Added getCountryData function to get and display different country information

--- script.js
+++ script.js
@@ -4,23 +4,36 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
-request.send();
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-  const HTML = `<article class="country">
+    const HTML = `<article class="country">
           <img class="country__img" src="${data.flags.png}" />
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
             <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}</p>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}</p>
             <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
-            <p class="country__row"><span>💰</span>${data.currencies.EUR.name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies.EUR.name
+            }</p>
           </div>
         </article> `;
-        countriesContainer.insertAdjacentHTML("beforebegin", HTML);
-        countriesContainer.style.opacity=1;
-});
+    countriesContainer.insertAdjacentHTML('beforebegin', HTML);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData('portugal');
+
+getCountryData('germany');
+
+
+
+
