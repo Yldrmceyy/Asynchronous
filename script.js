ba4168d7773e3252ced1df13b29a627d197fb3e8
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-23 14:39:36 +0300

    responses converted to json format

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
 request.send();
 console.log(request.responseText);
 request.addEventListener('load', function () {
-  console.log(this.responseText);
+  
+
+  const data= JSON.parse(this.responseText);
+  console.log(data);
 });
  
