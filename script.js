6e710c3d1846a24d47984fc53252f2ae1958da05
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-23 14:34:19 +0300

    Updated AJAX request handling: Log response text on load event

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const request=new XMLHttpRequest();
-request.open('GET','https://restcountries.com/v3.1/name/portugal');
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
+request.send();
+console.log(request.responseText);
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+});
