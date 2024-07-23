77bed675f2b4d4d7b649ebfa2d79897257352698
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-23 15:30:19 +0300

    Update the HTML template to correctly reference flags, languages, and currencies.

--- script.js
+++ script.js
@@ -12,14 +12,15 @@ request.addEventListener('load', function () {
   console.log(data);
 
   const HTML = `<article class="country">
-          <img class="country__img" src="${data.flags}" />
+          <img class="country__img" src="${data.flags.png}" />
           <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
+            <h3 class="country__name">${data.name.common}</h3>
             <h4 class="country__region">${data.region}</h4>
             <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}</p>
             <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
-            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies.EUR.name}</p>
           </div>
         </article> `;
-       
+        countriesContainer.insertAdjacentHTML("beforebegin", HTML);
+        countriesContainer.style.opacity=1;
 });
