c90649c1e737e26e2f31442a12e2bfc808dd4311
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-24 18:23:57 +0300

    Replace XMLHttpRequest with Fetch API to fetch country data

--- script.js
+++ script.js
@@ -40,7 +40,34 @@ getCountryData('usa');
 getCountryData('germany');
 */
 
+//Welcome to Callback Hell
+const renderCountry = function (data, className = '') {
+  const language = data.languages
+    ? Object.values(data.languages)[0]
+    : 'No data';
+  const currency = data.currencies
+    ? Object.values(data.currencies)[0].name
+    : 'No data';
+  const HTML = `<article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${language}</p>
+    <p class="country__row"><span>💰</span>${currency}</p>
+  </div>
+</article> `;
+  countriesContainer.insertAdjacentHTML('beforeend', HTML);
+  countriesContainer.style.opacity = 1;
+};
+
 //Consuming Promise with FETCH API
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
-}
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+getCountryData('portugal');
